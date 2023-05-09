2f242335c7fbca83696789b9c31ef65b67f48bcc
uhorn <uwehornnet@users.noreply.github.com>
2023-05-09 15:27:05 +0200

    updated baseURI

--- src/utils/baseURI.js
+++ src/utils/baseURI.js
@@ -1,1 +1,1 @@
-export const baseURI = process.env.NODE_ENV === "development" ? "http://localhost:8088" : "http://api.dapa-hst.de";
+export const baseURI = process.env.NODE_ENV === "development" ? "http://localhost:8088" : "http://dapa.uwe-horn.net";
